4a5ce5c61cde4cf6edca62265b1ce2b487b2062f
G Venkata Rama Krishna Prasad <glpsk369@gmail.com>
2025-02-18 22:38:35 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+region = "us-east-1"
+environment = "test"
+cidr_block = "11.0.0.0/16"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "value of region"
+  type        = string
+}
+variable "cidr_block" {
+  description = "value of cidr_block"
+  type        = string
+}
+variable "environment" {
+  description = "value of environment"
+  type        = string
+}
