19d6b5523562b5a46711f4f1250ca9619dc3a5ba
G Venkata Rama Krishna Prasad <glpsk369@gmail.com>
2025-02-18 22:59:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "region" {
   description = "value of region"
   type        = string
+  default     = "us-east-1"
 }
 variable "cidr_block" {
   description = "value of cidr_block"
   type        = string
+  default     = "11.0.0.0/16"
 }
 variable "environment" {
   description = "value of environment"
   type        = string
+  default     = "test"
 }
