01cfc5c522d8d46ac54bb9e2500bf88afca1efd4
G Venkata Rama Krishna Prasad <glpsk369@gmail.com>
2025-02-18 22:41:21 +0530

    Add files via upload

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "environment" {
+  description = "Name of the environment"
+  type        = string
+}
