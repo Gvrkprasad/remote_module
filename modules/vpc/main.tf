3d0730bd65ca1480f77d0b20f96a4c9b703bbde1
Gvrkprasad <glpsk369@gmail.com>
2025-02-19 13:54:50 +0530

    module_vpc Directory deleted

--- modules/vpc/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "eks" {    
-  cidr_block = var.cidr_block
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.environment}_vpc"
-  }
-
-}
--- modules/vpc/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-region = "us-east-1"
-environment = "test"
-cidr_block = "11.0.0.0/16"
